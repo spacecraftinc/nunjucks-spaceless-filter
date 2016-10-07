cb01d7fca8267cb7afaf64802dc263d55f53e5f7
Rob Lifford <rob@lifford.org>
2016-10-07 10:45:52 -0500

    add initial tests

--- test/index.js
+++ test/index.js
@@ -3,16 +3,25 @@
 /**
  * Imports
  */
-// const assert = require('assert');
-// const spaceless = require('../');
+const assert = require('assert');
+const spaceless = require('../');
 
-// describe('spaceless(value)', () => {
-//   it('should strip space and line breaks before and after the string', () => {
+describe('spaceless(value)', () => {
+  it('should strip space and line breaks before and after the string', () => {
+    const val = `
+          <div class="myDiv"><p class="myParagraph">Some text</p></div>
+    `;
+    const formatted = '<div class="myDiv"><p class="myParagraph">Some text</p></div>';
+    assert.strictEqual(spaceless(val), formatted);
+  });
 
-//   });
-
-//   it('should strip space and line breaks between tags', () => {
-
-
-//   });
-// });
+  it('should strip space and line breaks between tags', () => {
+    const val = `<div class="myDiv">
+      <p class="myParagraph">
+        <span>Some text</span>
+      </p>
+    </div>`;
+    const formatted = '<div class="myDiv"><p class="myParagraph"><span>Some text</span></p></div>';
+    assert.strictEqual(spaceless(val), formatted);
+  });
+});
