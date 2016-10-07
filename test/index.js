58a132dfe218de0a1b4626a7a1c6db746ab2f4aa
Rob Lifford <rob@lifford.org>
2016-10-07 11:22:25 -0500

    update circle.yml with token, update tests and test rules

--- test/index.js
+++ test/index.js
@@ -3,17 +3,32 @@
  */
 var assert = require('assert');
 var spaceless = require('../');
+var nunjucks = require('nunjucks');
+var env = new nunjucks.Environment([], { autoescape: false });
+env.addFilter('spaceless', spaceless);
+
 
 describe('spaceless(value)', function suite() {
-  it('should strip space and line breaks before and after the string', function test() {
-    var val = '    \n\n      <div class="myDiv"><p class="myParagraph">Some text</p></div>  \n\n    ';
-    var formatted = '<div class="myDiv"><p class="myParagraph">Some text</p></div>';
-    assert.strictEqual(spaceless(val), formatted);
+  it('shoud remove whitespace between HTML tags', function () {
+    assert.strictEqual(env.renderString('{% filter spaceless %}<div>\n\t \t\n</div>{% endfilter %}'), '<div></div>');
+    assert.strictEqual(env.renderString('{% filter spaceless %}<textarea>\n\t \t\n</textarea>{% endfilter %}'), '<textarea></textarea>');
+    assert.strictEqual(env.renderString('{% filter spaceless %}<pre>\n\t \t\n</pre>{% endfilter %}'), '<pre></pre>');
   });
 
-  it('should strip space and line breaks between tags', function test() {
-    var val = '<div class="myDiv">\n      <p class="myParagraph">\n        <span>Some text</span>\n      </p>\n    </div>';
-    var formatted = '<div class="myDiv"><p class="myParagraph"><span>Some text</span></p></div>';
-    assert.strictEqual(spaceless(val), formatted);
+  it('should remove whitespace around HTML tags', function () {
+    assert.strictEqual(env.renderString('{% filter spaceless %}\n\n\t\t  <div></div>\n\t {% endfilter %}'), '<div></div>');
+  });
+
+  it('should maintail whitespace outside `spaceless` tags', function () {
+    assert.strictEqual(env.renderString('\n\n\t\t  {% filter spaceless %}<div></div>{% endfilter %} \t\n'), '\n\n\t\t  <div></div> \t\n');
+  });
+
+  it('should maintain whitespace between HTML tag and text', function () {
+    assert.strictEqual(env.renderString('{% filter spaceless %}<div>\n\t <i>\n\t text </i> \t\n</div>{% endfilter %}'), '<div><i>\n\t text </i></div>');
+    assert.strictEqual(env.renderString('{% filter spaceless %}<pre>\n\t <i>\n\t text </i> \t\n</pre>{% endfilter %}'), '<pre><i>\n\t text </i></pre>');
+  });
+
+  it('should maintain whitespace inside tag', function () {
+    assert.strictEqual(env.renderString('{% filter spaceless %}<input name=" spaceless "  value="  "  required="  ">{% endfilter %}'), '<input name=" spaceless "  value="  "  required="  ">');
   });
 });
