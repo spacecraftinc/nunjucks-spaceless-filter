e5440f2e2a447aa0d4270049eb4f8f173931aab2
Rob Lifford <rob@lifford.org>
2016-10-07 11:01:07 -0500

    convert to ES5/node 0.10.x

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 /**
  * Constants
  */
-const WHITESPACE_AT_START = /^\s+/;
-const WHITESPACE_BETWEEN_TAGS = />\s+</g;
-const WHITESPACE_AT_END = /\s+$/;
+var WHITESPACE_AT_START = /^\s+/;
+var WHITESPACE_BETWEEN_TAGS = />\s+</g;
+var WHITESPACE_AT_END = /\s+$/;
 
 /**
  * Strips line breaks and space between tags in HTML/SVG
@@ -13,6 +13,9 @@ const WHITESPACE_AT_END = /\s+$/;
  * @param   {String}     value     source string
  * @returns {String}               formatted
  */
-module.exports = function(value) {
-  return value.replace(WHITESPACE_AT_START, '').replace(WHITESPACE_BETWEEN_TAGS, '><').replace(WHITESPACE_AT_END, '');
+module.exports = function spaceless(value) {
+  return value
+          .replace(WHITESPACE_AT_START, '')
+          .replace(WHITESPACE_BETWEEN_TAGS, '><')
+          .replace(WHITESPACE_AT_END, '');
 };
--- test/index.js
+++ test/index.js
@@ -1,27 +1,19 @@
-'use strict';
-
 /**
  * Imports
  */
-const assert = require('assert');
-const spaceless = require('../');
+var assert = require('assert');
+var spaceless = require('../');
 
-describe('spaceless(value)', () => {
-  it('should strip space and line breaks before and after the string', () => {
-    const val = `
-          <div class="myDiv"><p class="myParagraph">Some text</p></div>
-    `;
-    const formatted = '<div class="myDiv"><p class="myParagraph">Some text</p></div>';
+describe('spaceless(value)', function suite() {
+  it('should strip space and line breaks before and after the string', function test() {
+    var val = '    \n\n      <div class="myDiv"><p class="myParagraph">Some text</p></div>  \n\n    ';
+    var formatted = '<div class="myDiv"><p class="myParagraph">Some text</p></div>';
     assert.strictEqual(spaceless(val), formatted);
   });
 
-  it('should strip space and line breaks between tags', () => {
-    const val = `<div class="myDiv">
-      <p class="myParagraph">
-        <span>Some text</span>
-      </p>
-    </div>`;
-    const formatted = '<div class="myDiv"><p class="myParagraph"><span>Some text</span></p></div>';
+  it('should strip space and line breaks between tags', function test() {
+    var val = '<div class="myDiv">\n      <p class="myParagraph">\n        <span>Some text</span>\n      </p>\n    </div>';
+    var formatted = '<div class="myDiv"><p class="myParagraph"><span>Some text</span></p></div>';
     assert.strictEqual(spaceless(val), formatted);
   });
 });
